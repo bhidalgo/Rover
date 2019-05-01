0eaa80c51f7b5116fd6d7e939d785c4a4e34618a
brandon <bhidalgo1999@gmail.com>
2019-04-30 23:25:18 -0400

    [rover photos]
    fixing crash when picasso tries to load an image offline or with cleartext url.

--- app/src/main/java/com/example/rover/fragment/DetailFragment.kt
+++ app/src/main/java/com/example/rover/fragment/DetailFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.rover.R
@@ -55,7 +56,18 @@ class DetailFragment : Fragment(), CoroutineScope {
 
     private fun loadFullImage(imgSrc: String) {
         launch {
-            val rawBitmap = Picasso.get().load(imgSrc).get()
+            val parsedImageSource = imgSrc.replace("http:", "https:")
+            val rawBitmap = runCatching {
+                Picasso.get().load(parsedImageSource).get()
+            }.getOrNull()
+
+            if (rawBitmap == null) {
+                withContext(Dispatchers.Main) {
+                    Toast.makeText(requireContext(), "Could not load image.", Toast.LENGTH_SHORT).show()
+                }
+                return@launch
+            }
+
             withContext(Dispatchers.Main) {
                 val display = resources.displayMetrics
                 val screenDensity = resources.displayMetrics.density
